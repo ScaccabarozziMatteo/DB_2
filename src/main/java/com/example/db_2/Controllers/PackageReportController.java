9b7ec3c4b22fd90f66a7caf1b5487060f1e461db
Matteo Scaccabarozzi <matteo3.scaccabarozzi@mail.polimi.it>
2021-11-29 22:54:44 +0100

    Update PackageReportController.java

--- src/main/java/com/example/db_2/Controllers/PackageReportController.java
+++ src/main/java/com/example/db_2/Controllers/PackageReportController.java
@@ -5,6 +5,7 @@ import com.example.db_2.Services.MessageException;
 import com.example.db_2.Services.PackageReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@RestController(value="/packreport")
+@RestController
+@RequestMapping(value="/packreport")
 public class PackageReportController {
     private PackageReportService packageReportService;
 
